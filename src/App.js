c60c52c557f6f5691547d63588a6b0c16930ec3d
Barnabas Babatunde <babatundebarnabas@gmail.com>
2020-12-31 18:20:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,78 @@
-//FINAL FILE
-
-/*import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super (props);
+    this.brightTheme = this.brightTheme.bind(this);
+    this.darkTheme = this.darkTheme.bind(this);
+    this.updateThemeButtons = this.updateThemeButtons.bind(this);
+
+    this.state = {
+      theme: "bright",
+      themeButtons: []
+    }
+  }
+  brightTheme(){
+    const light = "bright";
+    this.setState({theme: light});
+  }
+  darkTheme(){
+    const noLight = "dark";
+    this.setState({theme: noLight});
+  }
+  updateThemeButtons(){
+    const updatedThemeButtons = ["Dark theme", "Bright theme"];
+    this.setState({themeButtons: updatedThemeButtons});
+  }
+  render(){
+    return (
+      <div className="wrapper">
+        <div id={this.state.theme} className="screen">
+          <h1 className="heading">React.js</h1>
+          <article>
+            React (also known as React.js or ReactJS) is an open-source, front end, JavaScript library
+            for building user interfaces or UI components. It is maintained by Facebook and a community 
+            of individual developers and companies. React can be used as a base in the development 
+            pf single-page or mobile applications. However, React is only concerned with the state management 
+            and rendering that state to the DOM, so creating React applications usually requires use of 
+            additional libraries for routing. React Router is an example of such a library.
+            <br/><br/>
+            React (also known as React.js or ReactJS) is an open-source, front end, JavaScript library
+            for building user interfaces or UI components. It is maintained by Facebook and a community 
+            of individual developers and companies. React can be used as a base in the development 
+            pf single-page or mobile applications. However, React is only concerned with the state management 
+            and rendering that state to the DOM, so creating React applications usually requires use of 
+            additional libraries for routing. React Router is an example of such a library.
+          </article>
+        </div>
+        <section className="buttons-section">
+          <button onClick={()=>{this.updateThemeButtons()}} className="change-theme">Change theme</button>
+          {
+            (()=>{
+              if(this.state.themeButtons[0]){
+                if(this.state.themeButtons[1]){
+                  return (
+                    <div className="theme-buttons">
+                      <button
+                      onClick={()=>{this.darkTheme()}}
+                      className="dark-theme"
+                      >{this.state.themeButtons[0]}</button>
+
+                      <button
+                      onClick={()=>{this.brightTheme()}}
+                      className="bright-theme"
+                      >{this.state.themeButtons[1]}</button>
+                    </div>
+                  )
+                }
+              }
+            })()
+          }
+        </section>
+      </div>
+    )
+  }
 }
 
-export default App;*/
-
-
-import Form from './Form';
-import Note from './Note';
-import './App.css';
-
-const App = ()=>{
-  return (
-    <div className="app">
-      <h1 className="heading">Express Yourself!</h1>
-      <Form />
-      <Note />
-    </div>
-  )
-}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,22 @@
 //ENTRY POINT
-
-import React from 'react';
+import {React} from 'react';
 import ReactDOM from 'react-dom';
-import AppLogic from './AppLogic';
 import App from './App';
-import Preload from './Preload';
 import reportWebVitals from './reportWebVitals';
-import PreloadLogic from './PreloadLogic';
-
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Preload />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 )
-PreloadLogic();
-
-
-const render = ()=>{
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-  AppLogic();
-}
-
-//Asynchronous rendering
-setTimeout(render, 5000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+if(module.hot){
+  module.hot.accept('./App.js', function hotReload(){
+    console.log('Accepting the updated App module!');
+    App();
+  });
+}
